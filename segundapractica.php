2b9a898ae15c95a6ada59745298159482e918d58
JazLuv <al02879683@tecmilenio.mx>
2025-01-30 19:32:49 -0600

    Segunda practica con funciones terminada

--- segundapractica.php
+++ segundapractica.php
@@ -11,4 +11,31 @@ function suma($x, $y){
     return $x + $y;
 }
 echo "<p>".suma(5, 6)."</p>";
+
+$saludo = function($hola){
+    echo "<p>Hola me llamo $hola</p>";
+};
+echo $saludo ("Aaron");
+
+$multiplicar = fn($x, $y) => $x * $y;
+echo "<p>". $multiplicar(12, 25)."</p>";
+
+$variable_global = 15;
+function miFunction() {
+    global $variable_global;
+    echo "<p>".$variable_global."</p>";
+    $variable_global++;
+}
+miFunction();
+echo "<p>".$variable_global."</p>";
+
+function sumaContador(){
+    static $contador = 0;
+    $contador++;
+    echo "<p>".$contador."</p>";
+}
+echo "<p>".sumaContador()."</p>";
+echo "<p>".sumaContador()."</p>";
+echo "<p>".sumaContador()."</p>";
+
 ?>
